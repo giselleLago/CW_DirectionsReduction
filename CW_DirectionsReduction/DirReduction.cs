7749a8896cde43cdef15399f7f571bfec852f355
giselleLago <gisellelago25994@gmail.com>
2020-08-12 12:34:48 +0200

    Add project files.

---
+++ CW_DirectionsReduction/DirReduction.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_DirectionsReduction
+{
+    public class DirReduction
+    {
+        public string[] dirReduc(String[] arr)
+        {
+            var lastDir = "";
+            var list = arr.ToList();
+            var isNull = new string[] { "NORTH", "SOUTH", "WEST", "EAST" };
+            for (int i = 0; i < list.Count; i++)
+            {
+                switch (list[i])
+                {
+                    case "NORTH":
+                        if (lastDir == "SOUTH")
+                        {
+                            list.Remove(list[i]);
+                            i--;
+                            list.Remove(list[i]);
+                            i--;
+                            if (i < 0)
+                            {
+                                i = 0;
+                            }
+                            lastDir = list[i];
+                            break;
+                        }
+                        if (list.Count == 0)
+                        {
+                            return isNull;
+                        }
+                        lastDir = list[i];
+                        break;
+
+                    case "SOUTH":
+                        if (lastDir == "NORTH")
+                        {
+                            list.Remove(list[i]);
+                            i--;
+                            list.Remove(list[i]);
+                            i--;
+                            if (i < 0)
+                            {
+                                i = 0;
+                            }
+                            lastDir = list[i];
+                            break;
+                        }
+                        if (list.Count == 0)
+                        {
+                            return isNull;
+                        }
+                        lastDir = list[i];
+                        break;
+
+                    case "WEST":
+                        if (lastDir == "EAST")
+                        {
+                            list.Remove(list[i]);
+                            i--;
+                            list.Remove(list[i]);
+                            i--;
+                            if (i < 0)
+                            {
+                                i = 0;
+                            }
+                            if (list.Count == 0)
+                            {
+                                return isNull;
+                            }
+                            lastDir = list[i];
+                            break;
+                        }
+                        lastDir = list[i];
+                        break;
+
+                    default:
+                        if (lastDir == "WEST")
+                        {
+                            list.Remove(list[i]);
+                            i--;
+                            list.Remove(list[i]);
+                            i--;
+                            if (i < 0)
+                            {
+                                i = 0;
+                            }
+                            if (list.Count == 0)
+                            {
+                                return isNull;
+                            }
+                            lastDir = list[i];
+                            break;
+                        }
+                        lastDir = list[i];
+                        break;
+                }
+            }
+            return list.ToArray();
+        }
+    }
+}
---
+++ CW_DirectionsReduction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_DirectionsReduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new DirReduction();
+            Console.WriteLine(a.dirReduc(new string[] { "NORTH", "SOUTH", "WEST", "EAST" }));
+            Console.ReadKey();
+        }
+    }
+}
